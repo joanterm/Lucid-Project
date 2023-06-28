1aa1691fc53c38ce4060438960e0702f7e57542c
Nelson Maldonado <play2nino@gmail.com>
2023-06-28 16:06:42 -0700

    added the app title on the left top corner

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StyleSheet, Text, View, Image, StatusBar } from "react-native"
-import { Button } from "@react-native-material/core"
+import { Button, Stack } from "@react-native-material/core"
+
 import bg from "./assets/backgrounds/dreamBG.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={bg} style={styles.backgroundImage} />
-      <Button title="SETTINGS" />
-      <Button title="START" />
-      <Button title="CUSTOM" />
-
+      <Text style={styles.texto}>Dream Nav</Text>
+      <Stack fill in center spacing={20}>
+        <Button title="SETTINGS" />
+        <Button title="START" />
+        <Button title="CUSTOM" />
+      </Stack>
       <StatusBar
         barStyle="light-content" // Set the desired status bar style
       />
@@ -32,5 +35,12 @@ const styles = StyleSheet.create({
     left: 0,
     width: "100%",
     height: "100%",
+  },
+  texto: {
+    color: "white",
+    marginTop: 50,
+    marginRight: 200,
+    fontSize: 25,
+    fontWeight: 600,
   },
 })
