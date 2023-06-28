ca7f868d5fa416945e3441a8264f81bf44e41951
Nelson Maldonado <play2nino@gmail.com>
2023-06-28 14:25:09 -0700

    added background, centered it, covered style

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View, Image } from "react-native"
+import bg from "./assets/backgrounds/dreamBG.png"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST</Text>
+      <Image source={bg} style={styles.backgroundImage} />
+      <Text>Start</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
+})
